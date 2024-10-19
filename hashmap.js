75bc1cbfd26b08b9293a84655ec93bba4881f10f
sykul <luke.flatley@outlook.com>
2024-10-19 22:41:32 +0200

    initial commit

---
+++ hashmap.js
@@ -0,0 +1,14 @@
+class HashMap {
+    constructor
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+        }
+
+        return hashCode;
+    }
+}
